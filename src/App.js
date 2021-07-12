86c2413abec17693aa0ea0dee184d0494296f350
AkshayChandra09 <akshay.chandrachood@gmail.com>
2021-07-12 16:34:49 -0500

    other front end related changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './components/Search';
 import Results from './components/Results';
+import MapContainer from './components/MapContainer';
 
 
 class App extends React.Component {
@@ -12,41 +13,57 @@ class App extends React.Component {
     currentLatitude: 0.00,
     currentLongitude: 0.00,
     searchQuery: '',
-    restaurants: [
-      {name: 'Tikka Taco', address: 'Market placed Blvd', ratings: '4/5'},
-      {name: 'Inchin Bamboo Garden', address: 'Market placed Blvd', ratings: '4.5/5'},
-      {name: 'India 101', address: 'Market placed Blvd', ratings: '4/5'},
-    ]
+    restaurants: []
   }
 
 
   // fix this part
-  componentDidMount() {
+  // componentDidMount() {
     // navigator.geolocation.getCurrentPosition(function (position) {
     //   console.log("Latitude is :", position.coords.latitude);
     //   console.log("Longitude is :", position.coords.longitude);
     // });
 
     // const apiUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=[AIzaSyBDUO5FgkAiT03CHEBMYQ75ZNF13ucAfyw]&location=-32.863708646699656,-96.95087568907348&radius=5000&type=restaurant'; 
-    // fetch(apiUrl)
+    // https://foodbukka.herokuapp.com/api/v1/restaurant?select=businessName, reviews, restauranttype&parkinglot=false&sort=businessName&page=2
+  // }
+
+  componentDidMount() {
+    // fetch('http://127.0.0.1:5000/get_restaurants?search='+this.state.searchQuery)
     //   .then(res => res.json())
-    //   .then((data) => console.log(data))
-    //   .catch(console.log);
+    //   .then(
+    //     (result) => {
+    //       this.setState({restaurants: result.results})
+    //     },
+    //     (error) => {
+    //       debugger;
+    //       console.log(error);
+    //     }
+    //   )
   }
+
 
   handleQuery = (query) => {
     this.setState({searchQuery: query});
+  }
+
+  getResults = (data) => {
+    debugger;
+    this.setState({restaurants:data.Results});
   }
 
   render() {
     return (
       <div className="col-md-12">
         <center><h1><i>Tengo Hambre..</i></h1></center>
-        <div class="col-md-4">
-          <Search onSearch={this.handleQuery} /><br />
-          <Results results={this.state.restaurants} />
-          {/* <Map /> */}
-          <p>{this.state.searchQuery}</p>
+        <div className="col-md-4">
+          <Search onSearch={this.handleQuery} getResults={this.getResults} /><br />
+          
+          {this.state.restaurants !== 'undefined' ?
+            <Results restaurants={this.state.restaurants} searchQuery={this.state.searchQuery} />
+            : <h4>No Records Found!</h4> 
+          }
+          {/* <MapContainer /> */}
         </div>
       </div>
     );
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Results = ({ results }) => {
-    return (
-        <div>
-            {results.map((restaurant) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{restaurant.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{restaurant.address}</h6>
-                        <p class="card-text">{restaurant.ratings}</p>
+export default class Results extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        const restaurants = this.props.restaurants;
+
+        // optimize this logic later
+        if(restaurants == 'undefined'){
+            debugger;
+            return null;
+        }
+
+        return (
+            <div>
+                <h4><i>Search Results for: {this.props.searchQuery}</i></h4>
+                {restaurants && restaurants.map((restaurant) => (
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{restaurant.Name}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{restaurant.Ratings}</h6>
+                            <p className="card-text">{restaurant.Type}</p>
+                            <p className="card-text">{restaurant.Address}</p>
+                            <p className="card-text">{restaurant.Hours}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    )
-};
-
-export default Results
+                ))
+                }     
+            </div>
+        )
+    }
+}
