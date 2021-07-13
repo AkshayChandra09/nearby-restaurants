2658080ba478ec094aeff6b506929375a399b7f6
AkshayChandra09 <akshay.chandrachood@gmail.com>
2021-07-13 12:55:56 -0500

    started working on scroll bar

--- src/components/Results.js
+++ src/components/Results.js
@@ -12,16 +12,18 @@ export default class Results extends Component {
         return (
             <div>
                 <h4><i>Search Results for: {this.props.searchQuery}</i></h4>
-                {restaurants && restaurants.map((restaurant) => (
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">{restaurant.name}</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">Ratings: {restaurant.rating}</h6>
-                            <p className="card-text">Address: {restaurant.vicinity}</p>
+                <div style={{"flex": "2", "height": "750px", "overflow-y": "scroll"}}>
+                    {restaurants && restaurants.map((restaurant) => (
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{restaurant.name}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">Ratings: {restaurant.rating}</h6>
+                                <p className="card-text">Address: {restaurant.vicinity}</p>
+                            </div>
                         </div>
-                    </div>
-                ))
-                }     
+                    ))
+                    }
+                </div>     
             </div>
         )
     }
